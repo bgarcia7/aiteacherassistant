37eaf62ccc8684ec4703a3a4efa32e101bdd1e87
Kykeon <paulinoandresmiguel@gmail.com>
2023-01-29 20:37:54 -0500

    Edit nav header

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,34 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/list',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+  '@fullcalendar/timeline',
+]);
 
-module.exports = nextConfig
+module.exports = withTM({
+  swcMinify: false,
+  trailingSlash: true,
+  env: {
+    // HOST
+    HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
+    // MAPBOX
+    MAPBOX_API: '',
+    // FIREBASE
+    FIREBASE_API_KEY: '',
+    FIREBASE_AUTH_DOMAIN: '',
+    FIREBASE_PROJECT_ID: '',
+    FIREBASE_STORAGE_BUCKET: '',
+    FIREBASE_MESSAGING_SENDER_ID: '',
+    FIREBASE_APPID: '',
+    FIREBASE_MEASUREMENT_ID: '',
+    // AWS COGNITO
+    AWS_COGNITO_USER_POOL_ID: '',
+    AWS_COGNITO_CLIENT_ID: '',
+    // AUTH0
+    AUTH0_DOMAIN: '',
+    AUTH0_CLIENT_ID: '',
+  },
+});
---
+++ client/src/redux/slices/calendar.js
@@ -0,0 +1,152 @@
+import { createSlice } from '@reduxjs/toolkit';
+// utils
+import axios from '../../utils/axios';
+
+// ----------------------------------------------------------------------
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  events: [],
+  openModal: false,
+  selectedEventId: null,
+  selectedRange: null,
+};
+
+const slice = createSlice({
+  name: 'calendar',
+  initialState,
+  reducers: {
+    // START LOADING
+    startLoading(state) {
+      state.isLoading = true;
+    },
+
+    // HAS ERROR
+    hasError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+
+    // GET EVENTS
+    getEventsSuccess(state, action) {
+      state.isLoading = false;
+      state.events = action.payload;
+    },
+
+    // CREATE EVENT
+    createEventSuccess(state, action) {
+      const newEvent = action.payload;
+      state.isLoading = false;
+      state.events = [...state.events, newEvent];
+    },
+
+    // UPDATE EVENT
+    updateEventSuccess(state, action) {
+      state.isLoading = false;
+      state.events = state.events.map((event) => {
+        if (event.id === action.payload.id) {
+          return action.payload;
+        }
+        return event;
+      });
+    },
+
+    // DELETE EVENT
+    deleteEventSuccess(state, action) {
+      const eventId = action.payload;
+      state.events = state.events.filter((event) => event.id !== eventId);
+    },
+
+    // SELECT EVENT
+    selectEvent(state, action) {
+      const eventId = action.payload;
+      state.openModal = true;
+      state.selectedEventId = eventId;
+    },
+
+    // SELECT RANGE
+    selectRange(state, action) {
+      const { start, end } = action.payload;
+      state.openModal = true;
+      state.selectedRange = { start, end };
+    },
+
+    // OPEN MODAL
+    onOpenModal(state) {
+      state.openModal = true;
+    },
+
+    // CLOSE MODAL
+    onCloseModal(state) {
+      state.openModal = false;
+      state.selectedEventId = null;
+      state.selectedRange = null;
+    },
+  },
+});
+
+// Reducer
+export default slice.reducer;
+
+// Actions
+export const { onOpenModal, onCloseModal, selectEvent, selectRange } = slice.actions;
+
+// ----------------------------------------------------------------------
+
+export function getEvents() {
+  return async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const response = await axios.get('/api/calendar/events');
+      dispatch(slice.actions.getEventsSuccess(response.data.events));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
+
+// ----------------------------------------------------------------------
+
+export function createEvent(newEvent) {
+  return async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const response = await axios.post('/api/calendar/events/new', newEvent);
+      dispatch(slice.actions.createEventSuccess(response.data.event));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
+
+// ----------------------------------------------------------------------
+
+export function updateEvent(eventId, event) {
+  return async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      const response = await axios.post('/api/calendar/events/update', {
+        eventId,
+        event,
+      });
+      dispatch(slice.actions.updateEventSuccess(response.data.event));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
+
+// ----------------------------------------------------------------------
+
+export function deleteEvent(eventId) {
+  return async (dispatch) => {
+    dispatch(slice.actions.startLoading());
+    try {
+      await axios.post('/api/calendar/events/delete', { eventId });
+      dispatch(slice.actions.deleteEventSuccess(eventId));
+    } catch (error) {
+      dispatch(slice.actions.hasError(error));
+    }
+  };
+}
