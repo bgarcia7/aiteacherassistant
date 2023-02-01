24c81753baab14e2be24559ec5436ad4269ca460
Kykeon <paulinoandresmiguel@gmail.com>
2023-01-31 23:19:00 -0500

    Test createLessonPlan api post, CORS not enabled

--- client/src/pages/api/Modules.js
+++ client/src/pages/api/Modules.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
-
 const API_URL = 'https://vjj6xrqlv1.execute-api.us-west-2.amazonaws.com/production/';
 
-export const getModule = async (moduleId) => {
-  try {
-    const response = await axios.get(`${API_URL}/${moduleId}`);
-    return response.data;
-  } catch (error) {
-    throw error;
+export const createLessonPlan = async (lessonPlan) => {
+  const response = await axios.post(`${API_URL}lesson_plan`, lessonPlan, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error(response.data.message);
   }
+  console.log(response.data);
+  return response.data;
 };
 
-export const getModules = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+const lessonPlan = {
+  title: 'Lesson Plan 1',
+  description: 'This is a lesson plan',
 };
+
+// createLessonPlan(lessonPlan);
