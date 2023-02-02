84f7ea5121a1617228f592117c11cbba014568c3
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-02 18:53:16 -0500

    Rework questions

--- client/src/sections/assessment/questions/Question3.js
+++ client/src/sections/assessment/questions/Question3.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { SeoIllustration } from 'src/assets/illustrations';
+import LoginAuth0 from 'src/sections/auth/LoginAuth0';
 
 const Question3 = () => {
   return (
     <Box>
-      <Typography variant="h6" gutterBottom component="div" textAlign={'center'}>
+      <LoginAuth0 />
+      {/* <Typography variant="h6" gutterBottom component="div" textAlign={'center'}>
         Awesome, we&apos;re almost done!
       </Typography>
       <Box display={'flex'} justifyContent={'center'} alignItems={'center'} py={5}>
@@ -20,7 +22,7 @@ const Question3 = () => {
       </Box>
       <Typography variant="h6" gutterBottom component="div" textAlign={'center'}>
         We&apos;re generating your lesson plans in the background. <br /> Click the finish button to login and view your lesson plans. 
-      </Typography>
+      </Typography> */}
     </Box>
   );
 };
