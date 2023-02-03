3ca408c6ddeabead2553ab7fec5d50640b52250c
Jorge <jsjfuentesj@gmail.com>
2023-02-03 03:36:10 -0500

    Disable eslint

--- client/next.config.js
+++ client/next.config.js
@@ -28,4 +28,9 @@ module.exports = withTM({
       ignoreDuringBuilds: true,
     },
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
