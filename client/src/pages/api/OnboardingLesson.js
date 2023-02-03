439f5cc18c22352be0d348978f130b86f1ab13d8
Kykeon <paulinoandresmiguel@gmail.com>
2023-02-03 01:45:34 -0500

    Test api - working

--- client/src/pages/api/OnboardingLesson.js
+++ client/src/pages/api/OnboardingLesson.js
@@ -4,21 +4,18 @@ const API_URL = 'https://vjj6xrqlv1.execute-api.us-west-2.amazonaws.com/producti
 
 export const createLessonPlan = async (lessonPlan) => {
   console.log('Sending lesson plan to server: ', lessonPlan[1].answers);
-  const { learning_objective, title } = lessonPlan[1].answers;
-  console.log('learning_objective: ', learning_objective);
-  console.log('title: ', title);
-  //   const response = await axios.post(`${API_URL}lesson_plan/`, lessonPlan, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
-  //       'Access-Control-Allow-Headers':
-  //         'X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization',
-  //       'Access-Control-Allow-Origin': '*',
-  //     },
-  //   });
-  //   if (response.status !== 200) {
-  //     throw new Error(response.data.message);
-  //   }
-  //   console.log('response', response.data);
-  return 'succesddds';
+  const response = await axios.post(`${API_URL}lesson_plan/`, lessonPlan[1].answers, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization',
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error(response.data.message);
+  }
+  console.log('response', response.data);
+  return response.data;
 };
